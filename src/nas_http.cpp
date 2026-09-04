6731079aa1396139e8c87b96e8b716a61d09fb61
Kian Gentry <musinks@pm.me>
2026-09-04 08:24:39 +0100

    nas_http: user ID and sessions

--- src/nas_http.cpp
+++ src/nas_http.cpp
@@ -1,6 +1,58 @@
 #include "mkwii/nas_http.h"
 
+#include <chrono>
+#include <ctime>
+#include <random>
+#include <string_view>
+#include <unordered_map>
+
 namespace mkwii {
+namespace {
+
+std::string random_text(std::size_t length) {
+    static std::mt19937 generator(std::random_device{}());
+    constexpr std::string_view alphabet =
+        "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    std::uniform_int_distribution<std::size_t> distribution(0, alphabet.size() - 1);
+
+    std::string value;
+    value.reserve(length);
+    for (std::size_t index = 0; index < length; ++index) {
+        value += alphabet[distribution(generator)];
+    }
+    return value;
+}
+
+std::string current_datetime() {
+    const std::time_t now = std::time(nullptr);
+    std::tm utc_time{};
+    gmtime_r(&now, &utc_time);
+
+    char value[15];
+    std::strftime(value, sizeof(value), "%Y%m%d%H%M%S", &utc_time);
+    return value;
+}
+
+std::string request_user_id(const std::string& request) {
+    const std::size_t body_start = request.find("\r\n\r\n");
+    const std::size_t user_id_start = request.find("userid=", body_start);
+    if (user_id_start == std::string::npos) {
+        return {};
+    }
+
+    const std::size_t value_start = user_id_start + 7;
+    const std::size_t value_end = request.find('&', value_start);
+    return request.substr(value_start, value_end - value_start);
+}
+
+struct LoginSession {
+    std::string challenge;
+    std::string token;
+};
+
+std::unordered_map<std::string, LoginSession> login_sessions;
+
+}  // namespace
 
 std::string nas_connectivity_response() {
     return "HTTP/1.1 200 OK\r\n"
@@ -19,14 +71,21 @@ std::string nas_response_for_request(const std::string& request) {
         return nas_connectivity_response();
     }
 
-    constexpr char login_body[] =
-        "retry=0&returncd=001&locator=gamespy.com&"
-        "challenge=kiwii-challenge&token=kiwii-token&"
-        "datetime=20260904000000";
+    const std::string user_id = request_user_id(request);
+    const LoginSession session{
+        random_text(8),
+        "NDS" + random_text(80),
+    };
+    login_sessions[user_id] = session;
+
+    const std::string login_body =
+        "retry=0&returncd=001&locator=gamespy.com&challenge=" +
+        session.challenge + "&token=" + session.token +
+        "&datetime=" + current_datetime();
     return "HTTP/1.1 200 OK\r\n"
            "Content-Type: text/plain\r\n"
-           "Content-Length: " + std::to_string(sizeof(login_body) - 1) +
-           "\r\n"
+           "Content-Length: " + std::to_string(login_body.size()) + "\r\n"
+           "NODE: wifiappe1\r\n"
            "Connection: close\r\n"
            "\r\n" + login_body;
 }
--- tests/nas_http_test.cpp
+++ tests/nas_http_test.cpp
@@ -16,10 +16,14 @@ int main() {
         "Host: naswii.nintendowifi.net\r\n\r\n"
         "action=bG9naW4%2A&userid=test";
     const std::string login_response = mkwii::nas_response_for_request(login_request);
-    assert(login_response.find("Content-Type: text/plain\r\n") != std::string::npos);
+        assert(login_response.find("Content-Type: text/plain\r\n") != std::string::npos);
+        assert(login_response.find("NODE: wifiappe1\r\n") != std::string::npos);
     assert(login_response.find("retry=0&returncd=001&locator=gamespy.com&") !=
            std::string::npos);
-    assert(login_response.ends_with("datetime=20260904000000"));
+        assert(login_response.find("&challenge=") != std::string::npos);
+        assert(login_response.find("&token=NDS") != std::string::npos);
+        assert(login_response.find("&datetime=") != std::string::npos);
+        assert(login_response.size() > response.size());
 
     assert(mkwii::nas_response_for_request("GET / HTTP/1.1\r\n\r\n") == response);
     return 0;
